1d71c9f38a1b0bee70931dc95ae0b3880ff9bce3
ardevtech7 <ardevtech7@gmail.com>
2023-01-05 17:52:50 +0900

    Servlet Study - 회원 관리 웹 어플리케이션 by servlet

---
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,50 @@
+package hello.servlet.domain.member;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class MemberRepositoryTest {
+    // 테스트할 대상 가져오기. Singleton 으로 생성했기 때문에 new 불가
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    // 테스트가 끝나면 초기화하기 위해 설정
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        // given
+        Member member = new Member("hello", 20);
+
+        // when
+        Member savedMember = memberRepository.save(member);
+
+        // then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+
+    @Test
+    void findAll() {
+        // given
+        Member member1 = new Member("member1", 10);
+        Member member2 = new Member("member2", 20);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // when
+        List<Member> result = memberRepository.findAll();
+
+        // then
+        assertThat(result.size()).isEqualTo(2);
+        org.assertj.core.api.Assertions.assertThat(result).contains(member1, member2);
+    }
+
+}
