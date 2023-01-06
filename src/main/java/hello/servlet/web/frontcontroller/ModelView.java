ec31faa3c9e57a5e147ab852a92e7a8fa53c00e0
ardevtech7 <ardevtech7@gmail.com>
2023-01-06 13:39:04 +0900

    Servlet Study - MVC Framework 만들기(Front Controller V1 ~ V5)

---
+++ src/main/java/hello/servlet/web/frontcontroller/ModelView.java
@@ -0,0 +1,36 @@
+package hello.servlet.web.frontcontroller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+    AS-IS
+    - request.setAttribute("member", member); 데이터를 담아서 view 에서 랜더링하던 방식
+
+    TO-BE
+    - Model 객체를 대신 만들어서 request가 하던 역할을 수행하는 것으로 변경
+ */
+public class ModelView {
+    private String viewName;
+    private Map<String, Object> model = new HashMap<>(); // ("member", Member)
+
+    public ModelView(String viewName) {
+        this.viewName = viewName;
+    }
+
+    public String getViewName() {
+        return viewName;
+    }
+
+    public void setViewName(String viewName) {
+        this.viewName = viewName;
+    }
+
+    public Map<String, Object> getModel() {
+        return model;
+    }
+
+    public void setModel(Map<String, Object> model) {
+        this.model = model;
+    }
+}
---
+++ src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -0,0 +1,42 @@
+package hello.servlet.web.frontcontroller.v1;
+
+import hello.servlet.web.frontcontroller.v1.controller.MemberFormControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberListControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberSaveControllerV1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "FrontControllerServletV1", urlPatterns = "/front-controller/v1/*")
+public class FrontControllerServletV1 extends HttpServlet {
+    // mapping 정보: 어떤 url 이 호출되는 controllerV1을 꺼내서 호출하는 것
+    private Map<String, ControllerV1> controllerMap = new HashMap<>();
+
+    // servlet 이 처음 생성될 때, mapping 정보를 담는다.
+    public FrontControllerServletV1() {
+        controllerMap.put("/front-controller/v1/members/new-form", new MemberFormControllerV1());
+        controllerMap.put("/front-controller/v1/members/save", new MemberSaveControllerV1());
+        controllerMap.put("/front-controller/v1/members", new MemberListControllerV1());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("FrontControllerServletV1.service");
+
+        String requestURI = request.getRequestURI();
+
+        // ControllerV1 controller = new MemberListControllerV1();
+        ControllerV1 controller = controllerMap.get(requestURI);
+        if (controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        controller.process(request, response);
+    }
+}
